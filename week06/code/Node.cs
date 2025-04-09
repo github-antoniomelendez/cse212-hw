c4dc84ae1fffc363216ea1bb105b412665458184
Antonio Melendez <mel23001@byui.edu>
2025-04-09 12:31:04 -0600

    W6 code

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,12 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        //check if the value already exists
+        if (value == Data)
+        {
+            //value is already in the tree, don't do nothing
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +40,50 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+        // Check if the current node's data is equal to the value
+        if (value == Data)
+        {
+            return true;
+        }
+
+        // If the value is less than the current node's data, search in the left subtree
+        if (value < Data)
+        {
+            if (Left is null)
+            {
+                return false;  // If left is null, the value doesn't exist in this subtree
+            }
+            return Left.Contains(value);  // Recurse into the left child
+        }
+
+        // If the value is greater than the current node's data, search in the right subtree
+        if (value > Data)
+        {
+            if (Right is null)
+            {
+                return false;  // If right is null, the value doesn't exist in this subtree
+            }
+            return Right.Contains(value);  // Recurse into the right child
+        }
         return false;
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        //return 0; // Replace this line with the correct return statement(s)
+
+        // Base case: if the node is null, the height is 0
+        if (this == null) {
+            return 0;
+        }
+        
+        // Recursively find the height of the left and right subtrees
+        int leftHeight = (this.Left != null) ? this.Left.GetHeight() : 0;
+        int rightHeight = (this.Right != null) ? this.Right.GetHeight() : 0;
+        
+        // The height of the current node is 1 + the maximum of the heights of its subtrees
+        return 1 + Math.Max(leftHeight, rightHeight);
+
     }
 }
--- week06/code/Trees.cs
+++ week06/code/Trees.cs
@@ -49,5 +49,20 @@ public static class Trees
     private static void InsertMiddle(int[] sortedNumbers, int first, int last, BinarySearchTree bst)
     {
         // TODO Start Problem 5
+        // Base case: If the current range is invalid, do nothing
+        if (first > last)
+        {
+            return;
+        }
+
+        // Find the middle index
+        int mid = (first + last) / 2;
+
+        // Insert the middle element into the BST
+        bst.Insert(sortedNumbers[mid]);
+
+        // Recursively insert the left and right halves
+        InsertMiddle(sortedNumbers, first, mid - 1, bst);  // Left half
+        InsertMiddle(sortedNumbers, mid + 1, last, bst);   // Right half
     }
 }
